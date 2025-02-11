7cf2fee52c914ae18bb4d044ef0b618d37aaafda
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-02-11 14:15:11 +0530

    Exceptions Demo

---
+++ ExceptionsDemo1/Program.cs
@@ -0,0 +1,58 @@
+﻿using SimpleMathClassLibrary;
+namespace ExceptionsDemo1
+{
+    internal class Program // UI
+    {
+        static void Main(string[] args) // UI   SRP
+        {
+            // Accept 2 ints, find sum and display ... 
+            // Non-Zero, Non-Negative, Non-Odd
+
+            while (true)
+            {
+                try
+                {
+                    int fno, sno, sum;
+                    Console.Write("Enter First Number: ");
+                    fno = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Second Number: ");
+                    sno = int.Parse(Console.ReadLine());
+
+                    // open db conn
+                    // write / read
+                    // ex 
+                    // close db conn
+
+                    //sum = fno + sno; // BL
+                    sum = Calculator.FindSum(fno, sno);
+
+                    Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Enter only numbers");
+                }
+                //catch (OverflowException ex)
+                //{
+                //    Console.WriteLine("Enter only small numbers");
+                //}
+
+                catch (Exception ex) // Catch All Block
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    // resource management
+                    // close the db
+                    Console.WriteLine("I am in finally");
+                }
+            }
+        }
+
+
+    }
+
+
+
+}
---
+++ SimpleMathClassLibrary/Calculator.cs
@@ -0,0 +1,86 @@
+﻿namespace SimpleMathClassLibrary
+{
+
+    /// <summary>
+    /// Calculator is used for general mathematic calculations
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Finds the sum of two int numbers
+        /// </summary>
+        /// <param name="fno">first int number</param>
+        /// <param name="sno">second int number</param>
+        /// <returns>the sum of two numbers</returns>
+        /// <exception cref="UnableToFindSumException"></exception>
+        public static int FindSum(int fno, int sno) // BLL - SRP
+        {
+            //try
+            //{
+            //int a = 10;
+            //int b = 0;
+            //int c = a / b;
+
+            // non-zero
+            if (fno == 0 || sno == 0)
+            {
+                throw new InvalidInputException("Provide non zero input");
+            }
+
+            // negative no
+
+            if (sno < 0 || fno < 0)
+            {
+                throw new InvalidInputException("Provide non negative input");
+            }
+
+            // odd numbers
+            if (fno % 2 != 0 || fno % 2 != 0)
+            {
+                throw new InvalidInputException("Provide Even input");
+            }
+
+            return fno + sno;
+            // }
+            //catch (DivideByZeroException ex)
+            //{
+            //    //Console.WriteLine("");
+            //    //throw ex;
+
+            //    //1. Log 
+
+            //    // frameworks: Log4Net, nLog, SeriLog,.....
+
+            //    //2. Ex Convertion
+
+            //    UnableToFindSumException unableToFindSumException = new UnableToFindSumException("Database problem, please try later", ex);
+            //    throw unableToFindSumException;
+            //}
+        }
+    }
+
+    public class UnableToFindSumException : ApplicationException
+    {
+        //public UnableToFindSumException()
+        //{
+
+        //}
+        //public UnableToFindSumException(string msg) : base(msg)
+        //{
+        //    //this.Message = msg;
+        //}
+        public UnableToFindSumException(string msg = null, Exception innerExp = null) : base(msg, innerExp)
+        {
+
+        }
+    }
+
+    public class InvalidInputException : ApplicationException
+    {
+        public InvalidInputException(string msg = null) : base(msg)
+        {
+
+        }
+    }
+}
+
