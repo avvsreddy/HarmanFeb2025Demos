1de8a32ad641dcb1a667333f351667ff63e01934
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-02-12 10:37:03 +0530

    Delegate Demo1

---
+++ DelegatesDemo1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DelegatesDemo1
+{
+
+    //class MyDelegate : Delegate
+    //{
+
+    //}
+
+    delegate void MyDelegate(string str); // Step1: Declaration
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // call directly
+            // name
+            // sign
+            // 
+            //Greeting("Good Morning");
+
+            // indirectly
+            //Delegate obj = new Delegate();
+            //MyDelegate obj = new MyDelegate(Greeting);
+            Program p = new Program();
+            MyDelegate obj = new MyDelegate(p.Hello); // Step 2: Instantiation and initialization
+            obj += Greeting; // Subscription
+            obj -= p.Hello; // Unsubscription
+            //obj.Invoke("Hello"); // Step 3: Invocation
+            obj("helloooo");
+        }
+
+        static void Greeting(string text)
+        {
+            Console.WriteLine($"Greeting: {text}");
+        }
+
+        void Hello(string text)
+        {
+            Console.WriteLine($"Hello {text}");
+        }
+    }
+}
