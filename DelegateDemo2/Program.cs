c69d8111d3f70171553855b0613a7be0096aac2c
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-02-12 12:38:21 +0530

    Delegate Demo 3

--- DelegateDemo2/Program.cs
+++ DelegateDemo2/Program.cs
@@ -8,41 +8,113 @@ namespace DelegateDemo2
         {
             // Client Developer 1 -  all processes
             ProcessManager pMgr = new ProcessManager();
-            //pMgr.ShowProcessList();
+
+            //pMgr.ShowProcessList(NoFilter);
+
+            // Anonymous Delegates
+            //pMgr.ShowProcessList(delegate
+            //{
+            //    return true;
+            //});
+
+            // Anonymous Delegates => Lambda Expression
+            pMgr.ShowProcessList(p => true);
+
             // Client 2 - starts with S
             //pMgr.ShowProcessList("S");
+            //FilterDelegate filter = new FilterDelegate(FilterByName);
+            //pMgr.ShowProcessList(filter);
+            //pMgr.ShowProcessList(delegate (Process p)
+            //{
+            //    if (p.ProcessName.StartsWith("S"))
+            //        return true;
+            //    return false;
+            //});
+
 
             // Client 3
-            pMgr.ShowProcessList(100 * 1024 * 1024);
+            //pMgr.ShowProcessList(FilterBySize);
+            // Anonymouse Delegate
+
+            pMgr.ShowProcessList(delegate (Process p)
+            {
+                return p.WorkingSet64 >= 100 * 1024 * 1024;
+            });
+
+            // Lambda Expression => Light Weight Syntax for Anonymous Delegates
+
+
+            // Anonymouse Delegate => Lambda Statement
+
+            pMgr.ShowProcessList((Process p) =>
+            {
+                return p.WorkingSet64 >= 100 * 1024 * 1024;
+            });
+
+            // Anonymouse Delegate => Lambda Statement => Lambda Expression
+
+            pMgr.ShowProcessList((Process p) =>
+
+                 p.WorkingSet64 >= 100 * 1024 * 1024
+            );
+
+            pMgr.ShowProcessList(p => p.WorkingSet64 >= 100 * 1024 * 1024);
         }
+
+        // Client 1
+
+        //public static bool NoFilter(Process p)
+        //{
+        //    return true;
+        //}
+
+
+        // client 2
+        //public static bool FilterByName(Process p)
+        //{
+        //    if (p.ProcessName.StartsWith("S"))
+        //        return true;
+        //    return false;
+        //}
+
+        // client 3
+        //public static bool FilterBySize(Process p)
+        //{
+        //    return p.WorkingSet64 >= 100 * 1024 * 1024;
+        //}
     }
 
+
     // Dev 1
-    class ProcessManager
+    // declare
+
+    public delegate bool FilterDelegate(Process p);
+
+    class ProcessManager // OCP
     {
-        public void ShowProcessList()
+        //public void ShowProcessList()
+        //{
+        //    foreach (var p in Process.GetProcesses())
+        //    {
+        //        Console.WriteLine(p.ProcessName);
+        //    }
+        //}
+        public void ShowProcessList(FilterDelegate filter)
         {
             foreach (var p in Process.GetProcesses())
             {
-                Console.WriteLine(p.ProcessName);
-            }
-        }
-        public void ShowProcessList(string sw)
-        {
-            foreach (var p in Process.GetProcesses())
-            {
-                if (p.ProcessName.StartsWith(sw))
+                if (filter(p))
                     Console.WriteLine(p.ProcessName);
             }
         }
 
-        public void ShowProcessList(long size)
-        {
-            foreach (var p in Process.GetProcesses())
-            {
-                if (p.WorkingSet64 >= size)
-                    Console.WriteLine(p.ProcessName);
-            }
-        }
+        //public void ShowProcessList(long size)
+        //{
+        //    foreach (var p in Process.GetProcesses())
+        //    {
+        //        if (p.WorkingSet64 >= size)
+        //            Console.WriteLine(p.ProcessName);
+        //    }
+        //}
     }
 }
