3de05e8c82f0f7d69ceecc88ec3e5f261a5462ce
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2025-02-19 11:28:24 +0530

    EF Demo Code

---
+++ EFDemoApp/DataLayer/ProductsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EFDemoApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDemoApp.DataLayer
+{
+    public class ProductsDbContext : DbContext
+    {
+        // 1. Map to database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ProductsDb2025;Integrated Security=True");
+        }
+
+
+
+        // 2. Map to tables
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ EFDemoApp/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace EFDemoApp.Entities
+{
+    public class Product // Entity Class
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
